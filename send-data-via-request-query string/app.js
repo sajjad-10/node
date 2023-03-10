14990e8872185bcbfde971a0f7e47f00683fc0de
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-03-10 20:41:49 +0330

    Send data via Request - query string

---
+++ send-data-via-request-query string/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello express");
+}); // Every time requested to this rout ( / ) run function -- localhost:3000/
+
+app.get("/api/users", (req, res) => {
+    res.send([
+        { id: 1, name: "user1" },
+        { id: 2, name: "user2" },
+    ]);
+}); // -- localhost:3000/api/users
+
+app.get("/api/users/:id", (req, res) => {
+    // console.log(req.params);
+    res.send({ id: req.params.id, name: `user${req.params.id}` });
+}); // -- localhost:3000/api/users/2
+
+const port =  3000;
+app.listen(port, () => console.log(`Listening on port ${port}`)); // create web server
+
+/*
+ * run web server : node app
+ * in browser :
+ * * localhost:3000/
+ * * localhost:3000/api/user
+ */
