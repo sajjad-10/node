e8c454427d2b75b83ccf80ed9f9415a47be41e19
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-03-11 04:32:11 +0330

    get one user -- params

--- restful-api/app.js
+++ restful-api/app.js
@@ -10,5 +10,23 @@ app.get("/api/users", (req, res) => {
     });
 }); // localhost:3000/api/users
 
+/* * get one user -- params */
+app.get("/api/users/:id", (req, res) => {
+    const user = users.find((u) => {
+        return u.id === parseInt(req.params.id);
+    });
+    console.log(user);
+    if (!user) {
+        return res.json({
+            data: null,
+            massage: "The user with th given id was not found.",
+        });
+    } // if not find user
+    res.json({
+        data: user,
+        massage: "data send ( OK )",
+    });
+}); // localhost:3000/api/users/12
+
 const port = 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`)); // create web server
